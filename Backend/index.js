f6c977cc8ef5895ecf2d3372e213787426df95e5
Rashmi Gupta <rashmi.gupta7018@gmail.com>
2024-02-17 10:28:21 +0530

    add port

--- Backend/index.js
+++ Backend/index.js
@@ -8,7 +8,7 @@ const { createTodo, updateTodo } = require("./types");
 app.use(cors());
 app.use(express.json());
 
-const port = 3000;
+const PORT = process.env.PORT||3000;
 
 app.post("/todo", async (req, res) => {
   const createPayload = req.body;
@@ -110,6 +110,6 @@ app.put("/todo/:id", async (req, res) => {
   }
 });
 
-app.listen(port, () => {
+app.listen(PORT, () => {
   console.log("Server started on port", port);
 });
